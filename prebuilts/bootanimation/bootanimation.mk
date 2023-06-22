530dc0161ad2cb934731152a772586fae5dff11e
Thai Nguyen <thai_ng@outlook.com>
2023-06-21 23:04:12 -0700

    add bootanimation

---
+++ prebuilts/bootanimation/bootanimation.mk
@@ -0,0 +1,4 @@
+PRODUCT_COPY_FILES += \
+    vendor/surface/prebuilts/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
+    vendor/surface/prebuilts/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
+
